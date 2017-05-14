4f04cad5eb319a8c33cf7922c8f8668dd8a39e07
haz111 <haz111pl@gmail.com>
2017-05-15 00:02:50 +0200

    adjusted details to new domain

--- src/App.js
+++ src/App.js
@@ -19,8 +19,6 @@ class App extends Component {
         <Router>
           <Switch>
             <Route exact path="/" component={MenuCategory} />
-            <Route exact path="/helping_hand" component={MenuCategory} />
-            <Route path="/helping_hand/:category" component={PlacesPage} />
             <Route path="/:category" component={PlacesPage} />
           </Switch>
         </Router>
